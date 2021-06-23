5f3a828e29b21be788fc830bccb242749cb4029a
amy-chinlai <amy@actionnetwork.org>
2021-06-23 18:43:23 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,35 @@
 # Add  code here!
+require 'pry'
+
+def prime?(integer)
+    if integer > 1 && !divisible_by_two?(integer) && !divisible_by_three?(integer) && !ends_with_five?(integer)
+        true
+    else
+        false
+    end
+end
+
+def divisible_by_two?(integer)
+    if integer % 2 == 0
+        true
+    else
+        false
+    end
+end
+
+def divisible_by_three?(integer)
+    if integer.to_s.chars.map(&:to_i).sum % 3 == 0
+        true
+    else
+        false
+    end
+end
+
+def ends_with_five?(integer)
+    if integer > 5 && integer.to_s.chars.last.end_with?("5")
+        true
+    else
+        false
+    end
+end
+
